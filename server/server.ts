187563530478c4dd034484d8b85c2a06995bc556
Shubham-Tambei9 <tambeshubham2004@gmail.com>
2026-02-12 16:20:38 +0530

    Initial commit

---
+++ server/server.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import AuthRouter from './routes/AuthRoutes';
+import ThumbnailRouter from './routes/ThumbnailRoutes';
+import UserRouter from './routes/UserRoutes';
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string
+    }
+}
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5173','http://localhost:3000'],
+    credentials: true
+}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}, // 7 days
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}))
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is Live!');
+});
+
+app.use('/api/auth', AuthRouter)
+app.use('/api/thumbnail', ThumbnailRouter)
+app.use('/api/user', UserRouter)
+
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDB(); // connect to MongoDB first
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error('Failed to start server:', err);
+  }
+};
+
+startServer();
